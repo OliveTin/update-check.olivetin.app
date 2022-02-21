a9f532df00ac004a43b0198efeff4134cef50993
jamesread <contact@jwread.com>
2022-02-21 12:29:11 +0000

    Code formatting

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
+
 function parseVersionRequest() {
 	$req = file_get_contents('php://input', false, null, 0, 1000);
 	$json = json_decode($req);
@@ -22,28 +23,51 @@ function parseVersionRequest() {
 
 	// FIXME - could get any junk in the post body!
 
-	return $req;
+	return $json;
 }
 
 function storableVersionRequest($version) {
-	return date(DATE_ATOM) . ' , ' . $version . "\n";
+	return date(DATE_ATOM) . ' , ' . json_encode($version) . "\n";
 }
 
-function isNewVersionAvailable($req) {
-	echo 'none';
+function getUpdatedVersion($req) {
+	$latest = '2022-01-06';
+	
+	$versionMap = array();
+	$versionMap['dev'] = 'you-are-using-a-dev-build';
+	$versionMap['2021-07-19'] = $latest;
+	$versionMap['2021-11-19'] = $latest;
+	$versionMap[$latest] = 'none'; // You are using the latest version.
+
+	if (isset($versionMap[$req])) {
+		return $versionMap[$req];
+	}
+
+	return 'none'; // You are using an unknown version.
+}
+
+function isJson() {
+	$headers = getallheaders();
+
+	if (isset($headers['Content-Type'])) {
+		if ($headers['Content-Type'] == 'application/json') {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 try {
-	if (empty($_POST)) {
-		echo '<a href = "hhttps://www.olivetin.app/_update_checks_and_tracking.html">OliveTin Update Check Documentation</a>';
-		exit;
+	if (!isJson()) {
+		exit('<a href = "https://docs.olivetin.app/update-tracking.html">OliveTin Update Check Documentation</a>');
 	}
 
-	$version = parseVersionRequest();
+	$req = parseVersionRequest();
 		
-	file_put_contents('checkins.log', storableVersionRequest($version), FILE_APPEND);
+	file_put_contents('checkins.log', storableVersionRequest($req), FILE_APPEND);
 
-	echo isNewVersionAvailable($version);
+	echo getUpdatedVersion($req->CurrentVersion);
 } catch (Exception $e) {
 	echo $e;
 }
